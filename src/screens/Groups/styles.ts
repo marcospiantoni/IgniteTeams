8f2f0a2eb42d78005d35bf79fa5f9409e32e8f21
Marcos <marcospiantoni360@gmail.com>
2023-03-27 13:42:38 -0300

    Styled Components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #000;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+`;
+
+export const Title = styled.Text`
+    color: #fff;
+    font-size: 32;
+`;
