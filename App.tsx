04323e3d23043baaee5bcddd6b729653e89a5839
Marcos <marcospiantoni360@gmail.com>
2023-03-28 11:00:13 -0300

    Header component - @types-png.d.ts

--- App.tsx
+++ App.tsx
@@ -1,23 +1,26 @@
-import { Groups } from '@screens/Groups';
-import { Loading }  from '@components/Loading'
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components'
-import {useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import theme from './src/themes'
+import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import theme from "./src/themes";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-  <ThemeProvider theme={theme}>
-    <StatusBar
-      barStyle='light-content'
-      backgroundColor='transparent'
-      translucent
-    />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
-     {fontsLoaded ? <Groups/> : <Loading/> }
-  </ThemeProvider>
-   
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@components/Header";
 import * as Style from "./styles";
 
 export function Groups() {
   return (
     <Style.Container>
-  
+      <Header />
     </Style.Container>
   );
 }
