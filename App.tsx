e97407d5c3a6fd716b67943e26a43abf2759d20c
Marcos <marcospiantoni360@gmail.com>
2023-03-28 10:38:03 -0300

    StatusBar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Groups } from '@screens/Groups';
 import { Loading }  from '@components/Loading'
-
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components'
 import {useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import theme from './src/themes'
@@ -10,6 +10,12 @@ export default function App() {
 
   return (
   <ThemeProvider theme={theme}>
+    <StatusBar
+      barStyle='light-content'
+      backgroundColor='transparent'
+      translucent
+    />
+
      {fontsLoaded ? <Groups/> : <Loading/> }
   </ThemeProvider>
    
