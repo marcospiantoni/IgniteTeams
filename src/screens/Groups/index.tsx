f13fd3b339a274cdadeef1035dddcf2471fd990d
Marcos <marcospiantoni360@gmail.com>
2023-03-30 18:07:41 -0300

    Listing Groups - List Empty Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import * as Style from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(
+    [ 'Galera da Rocket',
+    
+    ]);
   return (
     <Style.Container>
       <Header />
@@ -11,8 +18,15 @@ export function Groups() {
         title="Turmas" 
         subTitle="Jogue com a sua turma!" />
 
-      <GroupCard 
-        title="Galera do Ignite!" />
+      <FlatList
+       data={groups}
+       keyExtractor={item => item}
+       renderItem= {({ item }) => (
+         <GroupCard 
+           title={item} 
+         />
+       )}
+      />
     </Style.Container>
   );
 }
