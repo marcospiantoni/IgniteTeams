2bd01b209bece181f02ef1c73cbed1d0ca72c105
Marcos <marcospiantoni360@gmail.com>
2023-04-04 15:23:16 -0300

    Handling Exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,14 +1,19 @@
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
+import { Alert } from "react-native";
 
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 import { Highlight } from "@components/Highlight";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
+
 import * as Style from "./styles";
+
+
 
 
 export function NewGroup() {
@@ -18,11 +23,20 @@ export function NewGroup() {
 
    async function handleNew() {
     try {
+      if(group.trim().length === 0 ) {
+        return  Alert.alert("Novo Grupo", "Informe um nome para a turma");
+      }
+      
       await groupCreate(group);
       navigation.navigate("players", { group });
 
     } catch (error) {
-      console.log(error);
+      if(error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
     }
    }
   
