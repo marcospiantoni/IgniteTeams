c6426ad6c7e8398900839fc0d09711caf00a3d5c
Marcos <marcospiantoni360@gmail.com>
2023-04-04 15:00:45 -0300

    custom error class

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -32,7 +32,6 @@ export function Groups() {
     }
 
     useFocusEffect(useCallback(() => {
-      console.log('useFocusEffect executou')
       fetchGroups();
     }, []));
 
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -3,9 +3,17 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { groupsGetAll } from "@storage/groupsGetAll";
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 
+import { AppError } from "@utils/AppError";
+
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll();
+
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError('Já existe um grupo cadastrado com esse nome!');
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup])
     await AsyncStorage.setItem(GROUP_COLLECTION, storage);
